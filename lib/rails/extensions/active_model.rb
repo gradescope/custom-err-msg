cdbb154d225d13ea98c8327ca49f18f3d32bec47
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2019-07-24 13:33:28 -0700

    Add back omitted line oops

--- lib/rails/extensions/active_model.rb
+++ lib/rails/extensions/active_model.rb
@@ -4,6 +4,7 @@ module ActiveModel
       return message if attribute == :base
       attr_name = attribute.to_s.gsub('.', '_').humanize
       attr_name = @base.class.human_attribute_name(attribute, :default => attr_name)
+      options = { :default => "%{attribute} %{message}", :attribute => attr_name }
       if message =~ /^\^/
         I18n.t(:"errors.format.full_message", options.merge(:message => m[1..-1], :default => "%{message}"))
       else
